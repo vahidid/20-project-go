8e785b2afc29a40d80cebee22355a56d3e0d11f6
Vahid Hasani <vahidhasani09@gmail.com>
2021-05-28 14:49:56 +0430

    add material ui base and configs

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./Theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
